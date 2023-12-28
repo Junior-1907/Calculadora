3ebdc86c79861d51f21e52380a67479f0d076374
A Junior <juninhokaiber@gmail.com>
2023-12-28 13:22:49 -0300

    Terminado o js da calculadora

--- normalCalc copy.js
+++ normalCalc copy.js
@@ -1,20 +1,104 @@
 let runningTotal = 0;
 var previuschar;
-var screen = 0;
+var buffer = 0;
 var symbol = "";
 
 const display = document.getElementById("display");
 
-function previusOperator(){
-    previuschar = screen.length - 1;
-    if(typeof previuschar === "number"){
-        console.log("não é")
-    } else{ console.log("é")}
-}
-function addToDisplay(value){
 
-screen += value;
-display.innerHTML = screen;
-screen = parseInt(screen);
-previusOperator();
+function addToDisplay(value) {
+    buffer += value;
+    if(value === '.'){
+        console.log(value)
+    }
+    else{
+        buffer = parseFloat(buffer);
+    }
+    
+    updateDisplay();
+    console.log(buffer);
 }
+
+function updateDisplay() {
+    display.innerHTML = buffer;
+}
+
+function makeOperation() {
+
+    runningTotal = parseFloat(runningTotal);
+    buffer = parseFloat(buffer);
+
+    switch (symbol) {
+        case '+':
+            buffer += runningTotal;
+            updateDisplay();
+            break;
+
+        case '-':
+            buffer += runningTotal;
+            updateDisplay();
+            break;
+
+        case '*':
+            buffer *= runningTotal;
+            updateDisplay();
+            break;
+
+        case '/':
+            runningTotal /= buffer;
+            buffer = runningTotal;
+            updateDisplay();
+            break;
+    }
+}
+function addSymbolToDisplay(value) {
+    switch (value) {
+        case '+':
+            runningTotal = buffer;
+            buffer = '+';
+            symbol = '+';
+            updateDisplay();
+            break;
+
+        case '-':
+            runningTotal = buffer;
+            buffer = '-';
+            symbol = '-';
+            updateDisplay();
+            break;
+
+        case 'x':
+            runningTotal = buffer;
+            buffer = '*';
+            symbol = '*';
+            updateDisplay();
+            buffer = 0;
+            break;
+
+        case '/':
+            runningTotal = buffer;
+            buffer = '/';
+            symbol = '/';
+            updateDisplay();
+            buffer = 0;
+            break;
+
+        case 'C':
+            buffer = 0;
+            runningTotal = 0;
+            updateDisplay();
+            break;
+
+        case '.':
+            buffer += '.' ;
+            buffer = parseFloat(buffer);
+            updateDisplay();
+            break;
+
+        case '=':
+            makeOperation();
+            updateDisplay();
+            break;
+    }
+}
+
